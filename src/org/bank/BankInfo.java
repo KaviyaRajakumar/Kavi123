46ce8f18c7b4c7ad30a4c22c5cb7aff500067abc
kavi123 <kavyaharshiraj@gmail.com>
2022-09-03 11:04:58 +0530

    Axis Bank

---
+++ src/org/bank/BankInfo.java
@@ -0,0 +1,16 @@
+package org.bank;
+
+public class BankInfo {
+	public void savings() {
+	System.out.println("6%");
+	}
+ public void deposit() {
+	 System.out.println("3%");
+	 
+	}
+ public void fixed() {
+	System.out.println("10%");
+
+}
+ 
+}
